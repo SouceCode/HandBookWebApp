56c9c00233946b9e5743c932ea84f7f47fe46857
爱悠的沫夏 <oncwnuMRLtgs-1vuQSn30RxDTltI@git.weixin.qq.com>
2019-11-08 15:06:35 +0800

    init

--- JS/global/fns/setting.js
+++ JS/global/fns/setting.js
@@ -1,7 +1,6 @@
-//JS 方法内部声明全局变量 配置API地址
+﻿//JS 方法内部声明全局变量 配置API地址
 function f_apiurl(){
-     // apiurl="http://localhost:5000";
-   apiurl="http://39.106.55.171:5000";
+     apiurl="http://localhost:5000";
  }
 
 
